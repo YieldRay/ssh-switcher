3be749c72a44c67953f89f6b6c04f47bf01ae5c2
YieldRay <yieldray@outlook.com>
2023-08-11 13:53:46 +0800

    support print key type

--- ssh-switcher.sh
+++ ssh-switcher.sh
@@ -86,8 +86,7 @@ setup_colors
 
 DATA_DIR="${XDG_CONFIG_HOME-$HOME/.config}/ssh-switcher"
 DATA_PROFILE_DIR="$DATA_DIR/profiles"
-FILE_RSA="$HOME/.ssh/id_rsa"
-FILE_RSA_PUB="$HOME/.ssh/id_rsa.pub"
+SSH_DIR="$HOME/.ssh"
 
 subcommand_save() {
     # verify args
@@ -96,19 +95,18 @@ subcommand_save() {
         echo
         usage
     fi
-    # ensure ssh key file exists
-    if [ ! -e "$FILE_RSA" ] && [ ! -e "$FILE_RSA_PUB" ]; then
-        msg "${RED}File \`$FILE_RSA\` and \`$FILE_RSA_PUB\` does not exists${NOFORMAT}"
+    # ensure ssh dir exists
+    if [ ! -d "$SSH_DIR" ]; then
+        msg "${RED}$SSH_DIR is not a directory${NOFORMAT}"
         exit 1
     fi
     # save action
     DATA_PROFILE_NAME_DIR="$DATA_PROFILE_DIR/$1"
     mkdir -p "$DATA_PROFILE_NAME_DIR"
-    cp "$FILE_RSA_PUB" "$DATA_PROFILE_NAME_DIR"
-    cp "$FILE_RSA" "$DATA_PROFILE_NAME_DIR"
+    cp -rv "$SSH_DIR"/id_* "$DATA_PROFILE_NAME_DIR"/ # only save ~/.ssh/id_*
     # if $2 is empty, print
     if [[ -z "${2-}" ]]; then
-        msg "${GREEN}Successfully save \`~/.ssh/id_rsa.pub\` and \`~/.ssh/id_rsa\` to \`$DATA_PROFILE_NAME_DIR\`${NOFORMAT}"
+        msg "${GREEN}Successfully saved \`$SSH_DIR/id_*\` to \`$DATA_PROFILE_NAME_DIR\`${NOFORMAT}"
     fi
 }
 
@@ -121,8 +119,8 @@ subcommand_load() {
     fi
     # ensure saved file exists
     DATA_PROFILE_NAME_DIR="$DATA_PROFILE_DIR/$1"
-    if [ ! -e "$DATA_PROFILE_NAME_DIR/id_rsa.pub" ] && [ ! -e "$DATA_PROFILE_NAME_DIR/id_rsa" ]; then
-        msg "${RED}File \`$DATA_PROFILE_NAME_DIR/id_rsa.pub\` and \`$DATA_PROFILE_NAME_DIR/id_rsa\` does not exists${NOFORMAT}"
+    if [ ! -d "$DATA_PROFILE_NAME_DIR" ]; then
+        msg "${RED}Profile does not exists${NOFORMAT}"
         exit 1
     fi
     # backup first (check if profile with current name exists)
@@ -130,17 +128,16 @@ subcommand_load() {
     if [ -f "$DATA_DIR/__current__" ]; then
         current_name=$(cat "$DATA_DIR/__current__")
     fi
-    if [ -z "$current_name" ] || [ ! -f "$DATA_PROFILE_DIR/$current_name/id_rsa.pub" ] || [ ! -f "$DATA_PROFILE_DIR/$current_name/id_rsa" ]; then
+    if [ -z "$current_name" ] || [ ! -d "$DATA_PROFILE_DIR/$current_name" ]; then
         backup_name="__backup_${RANDOM-random}__"
         subcommand_save "$backup_name" no_print
         msg "${BLUE}Profile has been backup as \`$backup_name\`${NOFORMAT}"
     fi
     # exec copy
-    cp "$DATA_PROFILE_NAME_DIR/id_rsa.pub" "$FILE_RSA_PUB"
-    cp "$DATA_PROFILE_NAME_DIR/id_rsa" "$FILE_RSA"
-    # store current name
+    cp -rv "$DATA_PROFILE_NAME_DIR"/* "$SSH_DIR"/
+    # update current name
     echo -n "$1" >"$DATA_DIR/__current__"
-    msg "${GREEN}Successfully load \`$1\`${NOFORMAT}"
+    msg "${GREEN}Successfully loaded \`$1\`${NOFORMAT}"
 }
 
 subcommand_remove() {
@@ -165,25 +162,31 @@ subcommand_remove() {
     done
     # exec
     if [ "$confirmation" = "y" ]; then
-        rm -rf "$DATA_PROFILE_NAME_DIR"
-        msg "${GREEN}Successfully remove \`$DATA_PROFILE_NAME_DIR\`${NOFORMAT}"
+        rm -rfv "$DATA_PROFILE_NAME_DIR" # remove entire dir
+        msg "${GREEN}Successfully removed \`$DATA_PROFILE_NAME_DIR\`${NOFORMAT}"
     else
-        msg "${RED}Exit${NOFORMAT}"
+        msg "${RED}Aborted${NOFORMAT}"
         exit 1
     fi
 }
 
 subcommand_list() {
     if [[ -d "$DATA_PROFILE_DIR" ]]; then
-        printf "\033[0;34m%20s %30s\033[0m\n" "Name" "Title"
-        printf "%20s %30s\n" "----" "-----"
+        printf "${BLUE}%20s %30s %15s${NOFORMAT}\n" "Name" "Title" "Type"
+        printf "%20s %30s %15s\n" "----" "-----" "----"
+
         for dir in "$DATA_PROFILE_DIR"/*; do
-            file="$dir/id_rsa.pub"
-            if [ -d "$dir" ] && [ -f "$file" ]; then
-                name=$(basename "$dir")
-                title=$(cut -d ' ' -f 3 <"$file")
-                printf "%20s %30s\n" "$name" "$title"
-            fi
+            if [ ! -d "$dir" ]; then continue; fi
+
+            find "$dir" -name "id_*.pub" -type f | while read -r file; do
+                if [[ "$(basename "$file")" =~ id_(.+)\.pub ]]; then
+                    type=${BASH_REMATCH[1]}           # profile type
+                    name=$(basename "$dir")           # profile name
+                    title=$(cut -d ' ' -f 3 <"$file") # profile title
+                    printf "%20s %30s %15s\n" "$name" "$title" "$type"
+                    break # only print the first ssh pub key
+                fi
+            done
         done
     else
         msg "${BLUE}Nothing to show${NOFORMAT}"
@@ -192,9 +195,10 @@ subcommand_list() {
 
 subcommand_whoami() {
     name="<none>"
-    if [ -f "$DATA_DIR/__current__" ]; then name=$(cat "$DATA_DIR/__current__"); fi
-    title=$(cut -d ' ' -f 3 <"$FILE_RSA_PUB")
-    msg "${name} ${ORANGE}${title}${NOFORMAT}"
+    if [ -f "$DATA_DIR/__current__" ]; then
+        name=$(cat "$DATA_DIR/__current__")
+    fi
+    msg "${name}"
 }
 
 case ${args[0]} in
